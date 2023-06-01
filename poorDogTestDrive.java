cabbd0eece89ceaf99ef66b22d1ae2ca5ff5d07d
Neto Pontes <70979963+netomaru@users.noreply.github.com>
2023-05-31 22:52:07 -0300

    Add files via upload

---
+++ poorDogTestDrive.java
@@ -0,0 +1,38 @@
+public class poorDogTestDrive
+{
+    public static void main(String[] args)
+    {
+        class poorDog
+        {
+            private int size;
+            private String name;
+
+            public int getSize()
+            {
+                return size;
+            }
+            public String getName()
+            {
+                return name;
+            }
+
+        }
+
+        poorDog one = new poorDog();
+        System.out.println("The size of dog is " + one.getSize());
+        System.out.println("The name of dog is " + one.getName());
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+}
